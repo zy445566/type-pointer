cd5183e2a97d142805d5c043831ff74b23d83ef6
ZhangYuan <zyct@vip.qq.com>
2018-03-28 18:32:06 +0800

    update

--- addon.cc
+++ addon.cc
@@ -1,37 +1,186 @@
 #include <nan.h>
+#include<sys/mman.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdio.h>
+#include<unistd.h>
 
-void getObjectAddr(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  if (info.Length() < 1) {
-    Nan::ThrowTypeError("Wrong number of arguments");
-    return;
-  }
-  char objAddrStr[20]; //max 0x8000000000000000
-  v8::Local<v8::Object> obj = info[0]->ToObject();
-  v8::Local<v8::Object> *objP;
-  objP = (v8::Local<v8::Object>*)malloc(sizeof(v8::Local<v8::Object>));
-  *objP = obj;
-  sprintf( objAddrStr,"%p",objP);
-  // sprintf( objAddrStr,"%p",&obj);
-  v8::Local<v8::String> v8ObjAddrStr = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), objAddrStr);
-  info.GetReturnValue().Set(v8ObjAddrStr);
+void * getEmptyPMap(const char* path,long unsigned int size)
+{
+    int fd;
+    fd = open(path,O_CREAT|O_RDWR|O_TRUNC,00777);
+    void * p_map;
+    p_map=(void *)mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    write(fd,"",1);
+    close(fd);
+    return p_map;
 }
 
-void getObjectByAddr(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  if (info.Length() < 1) {
+void * getPMap(const char* path,long unsigned int size)
+{
+    int fd;
+    void *  p_map;
+    fd = open(path,O_CREAT|O_RDWR,00777);
+    p_map = mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    close(fd);
+    return p_map;
+}
+
+void * getClosePMap(const char* path,long unsigned int size)
+{
+    void * p_map = getPMap(path,size);
+    munmap(p_map,size);
+    msync(p_map,size,MS_ASYNC);
+    return p_map;
+}
+
+v8::Local<v8::Object> *  getPMapByOffset(const char* path,long unsigned int size,long unsigned int offset)
+{
+    v8::Local<v8::Object> * p_map = (v8::Local<v8::Object> *)getPMap(path,size);
+    p_map += offset;
+    return p_map;
+}
+
+void * flushMem(const char* path,long unsigned int size)
+{
+    return getEmptyPMap(path,size);
+}
+
+
+void * closeMem(const char* path,long unsigned int size)
+{
+    return getClosePMap(path,size);
+}
+
+long unsigned int writeMem(const char* path,long unsigned int size,long unsigned int offset,v8::Local<v8::Object> obj)
+{
+    v8::Local<v8::Object> * p_map = getPMapByOffset(path,size,offset);
+    memcpy(p_map,&obj,sizeof(v8::Local<v8::Object>));
+    return offset;
+}
+
+v8::Local<v8::Object> readMem(const char* path,long unsigned int size,long unsigned int offset)
+{
+    v8::Local<v8::Object>  * p_map = getPMapByOffset(path,size,offset);
+    return *p_map;
+}
+
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value;
+}
+
+void typeFlush(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  if (info.Length() < 2) {
     Nan::ThrowTypeError("Wrong number of arguments");
     return;
   }
-  int64_t addr = info[0]->IntegerValue();
-  v8::Local<v8::Object> *objP;
-  objP = (v8::Local<v8::Object> *)addr;
-  info.GetReturnValue().Set(*objP);
+  if (!info[0]->IsString()) {
+    Nan::ThrowTypeError("path must be string");
+    return;
+  }
+  if (!info[1]->IsNumber()) {
+    Nan::ThrowTypeError("size must be number");
+    return;
+  }
+  v8::String::Utf8Value str0(info[0]->ToString());
+  const char* path= ToCString(str0);
+  uint32_t size = info[1]->Uint32Value();
+  void * p = flushMem(path,size);
+  char objAddrStr[20];
+  sprintf( objAddrStr,"%p",p);
+  v8::Local<v8::String> v8AddrStr = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), objAddrStr);
+  info.GetReturnValue().Set(v8AddrStr);
+}
+
+void typeClose(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  if (info.Length() < 2) {
+    Nan::ThrowTypeError("Wrong number of arguments");
+    return;
+  }
+  if (!info[0]->IsString()) {
+    Nan::ThrowTypeError("path must be string");
+    return;
+  }
+  if (!info[1]->IsNumber()) {
+    Nan::ThrowTypeError("size must be number");
+    return;
+  }
+  v8::String::Utf8Value str0(info[0]->ToString());
+  const char* path= ToCString(str0);
+  uint32_t size = info[1]->Uint32Value();
+  void * p = closeMem(path,size);
+  char objAddrStr[20];
+  sprintf( objAddrStr,"%p",p);
+  v8::Local<v8::String> v8AddrStr = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), objAddrStr);
+  info.GetReturnValue().Set(v8AddrStr);
+}
+
+void typeWrite(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  if (info.Length() < 2) {
+    Nan::ThrowTypeError("Wrong number of arguments");
+    return;
+  }
+  if (!info[0]->IsString()) {
+    Nan::ThrowTypeError("path must be string");
+    return;
+  }
+  if (!info[1]->IsNumber()) {
+    Nan::ThrowTypeError("size must be number");
+    return;
+  }
+  if (!info[2]->IsNumber()) {
+    Nan::ThrowTypeError("offset must be number");
+    return;
+  }
+  if (!info[3]->IsObject()) {
+    Nan::ThrowTypeError("obj must be object");
+    return;
+  }
+  v8::String::Utf8Value str0(info[0]->ToString());
+  const char* path= ToCString(str0);
+  uint32_t size = info[1]->Uint32Value();
+  uint32_t offset = info[2]->Uint32Value();
+  v8::Local<v8::Object> obj = info[3]->ToObject();
+  uint32_t writeOffset = writeMem(path,size,offset,obj);
+  v8::Local<v8::Uint32> resOffset= Nan::New(writeOffset);
+  info.GetReturnValue().Set(resOffset);
+}
+
+void typeRead(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  if (info.Length() < 2) {
+    Nan::ThrowTypeError("Wrong number of arguments");
+    return;
+  }
+  if (!info[0]->IsString()) {
+    Nan::ThrowTypeError("path must be string");
+    return;
+  }
+  if (!info[1]->IsNumber()) {
+    Nan::ThrowTypeError("size must be number");
+    return;
+  }
+  if (!info[2]->IsNumber()) {
+    Nan::ThrowTypeError("offset must be number");
+    return;
+  }
+  v8::String::Utf8Value str0(info[0]->ToString());
+  const char* path= ToCString(str0);
+  uint32_t size = info[1]->Uint32Value();
+  uint32_t offset = info[2]->Uint32Value();
+  v8::Local<v8::Object> obj = readMem(path,size,offset);
+  info.GetReturnValue().Set(obj);
 }
 
 void Init(v8::Local<v8::Object> exports) {
-  exports->Set(Nan::New("getObjectAddr").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(getObjectAddr)->GetFunction());
-  exports->Set(Nan::New("getObjectByAddr").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(getObjectByAddr)->GetFunction());
+  exports->Set(Nan::New("typeFlush").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(typeFlush)->GetFunction());
+  exports->Set(Nan::New("typeClose").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(typeClose)->GetFunction());
+  exports->Set(Nan::New("typeWrite").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(typeWrite)->GetFunction());
+  exports->Set(Nan::New("typeRead").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(typeRead)->GetFunction());
 }
 
 NODE_MODULE(typePointer, Init)
