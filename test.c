980448e4b6adc1977e01a5c05a4566e6fbac144b
ZhangYuan <zyct@vip.qq.com>
2018-03-28 13:03:38 +0800

    update

--- test.c
+++ test.c
@@ -5,32 +5,52 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int main()
+int typeFlush(const char* path,long unsigned int szie)
 {
+    szie = 4096;
     int fd;
-    char path[54] = "/Users/noder/Documents/GitHub/type-pointer/data/shareMem";
-    fd = open(path,O_CREAT|O_RDWR,00777);
-    double * p_map;
-    double val = 15.0;
-    p_map=(double *)mmap(NULL,1,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    write(fd,"",1);
+    fd = open(path,O_CREAT|O_RDWR|O_TRUNC,00777);
+    void * p_map;
+    p_map=(void *)mmap(NULL,szie,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    write(fd,"",szie);
     close(fd);
-    printf("%p\n",p_map);
-    printf("val:%f,%p,%lu\n",val,&val,sizeof(double));
-    memcpy(p_map,&val,sizeof(double));
-    printf("p_map:%f,%p,%lu\n",*p_map,p_map,sizeof(double));
-    munmap(p_map,1);
+    munmap(p_map,szie);
     return 0;
 }
 
-// int main()
-// {
-//     int fd;
-//     char path[54] = "/Users/noder/Documents/GitHub/type-pointer/data/shareMem";
-//     double *p_map;
-//     fd=open(path,O_CREAT|O_RDWR,00777);
-//     p_map=(double *)mmap(NULL,sizeof(double),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-//     printf("p_map2:%f;\n",*p_map);
-//     munmap(p_map,sizeof(double));
-//     return 0;
-// }
+long unsigned int typeWrite(const char* path,long unsigned int szie,long unsigned int offset,double obj)
+{
+    int fd;
+    double * p_map;
+    fd = open(path,O_CREAT|O_RDWR,00777);
+    p_map=(void *)mmap(NULL,szie,PROT_READ|PROT_WRITE,MAP_SHARED,fd,offset);
+    close(fd);
+    memcpy(p_map,&obj,sizeof(double));
+    munmap(p_map,sizeof(double));
+    return offset;
+}
+
+double typeRead(const char* path,long unsigned int szie,long unsigned int offset)
+{
+    int fd;
+    double * p_map;
+    fd = open(path,O_CREAT|O_RDWR,00777);
+    p_map=(void *)mmap(NULL,szie,PROT_READ|PROT_WRITE,MAP_SHARED,fd,offset);
+    close(fd);
+    return *p_map;
+}
+
+int main()
+{
+    const char path[60] = "/Users/noder/Documents/GitHub/type-pointer/data/shareMem";
+    long unsigned int szie = 4096;
+    double testObj1 = 888.0;
+    typeFlush(path,szie);
+    printf("test1\n");
+    long unsigned int  offset = typeWrite(path,szie,0,testObj1);
+    printf("test2\n");
+    double testObj2 = typeRead(path,szie,offset);
+    printf("test3\n");
+    printf("testObj2:%f\n",testObj2);
+    return 0;
+}
